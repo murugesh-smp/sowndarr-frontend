98f91a1a2457ac5ab795fa10799f367dace9c30b
sowndarr <95352430+sowndarr@users.noreply.github.com>
2021-12-01 17:18:47 +0530

    Add files via upload

---
+++ js/jquery.spectrum-fi.js
@@ -0,0 +1,14 @@
+// Spectrum Colorpicker
+// Finnish (fi) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["fi"] = {
+        cancelText: "",
+        chooseText: "Set Color"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
